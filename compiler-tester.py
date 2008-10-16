968abcb58b1b52570cfd8e7296a3b431aa359b16
Daniel Benamy <dbenamy@warsaw.clic.cs.columbia.edu>
2008-10-16 00:09:09 -0400

    Cleanup.

--- compiler-tester.py
+++ compiler-tester.py
@@ -11,17 +11,6 @@ def compile_and_run(compiler, code):
     the output of the program run.
     
     """
-    """
-    compiler_path = os.path.join('..', 'bin')
-    # TODO Make this work on other machines
-    classpaths = []
-    classpaths.append(os.path.join('c:', 'antlr', '277', 'lib', 'antlr.jar'))
-    classpaths.append(os.path.join('..', 'lib', 'jargs.jar'))
-    classpaths.append(os.path.join('..', 'lib', 'httpunit.jar'))
-    classpaths.append(os.path.join('..', 'bin'))
-    classpaths = ";".join(classpaths)
-    command = "java -classpath %s test.SpriteCompilerAndRunner" % classpaths
-    """
     stdin, stdouterr = os.popen4(compiler)
     stdin.write(code)
     stdin.close()
@@ -37,32 +26,6 @@ def print_indented(message):
 def run_test(compiler, code, correct_result):
     """Returns True if the test passes, otherwise False."""
     status, output = compile_and_run(compiler, code)
-    """
-    if correct_result == status or (status == 'GOOD' and
-                                    correct_result == output):
-        print "PASS"
-        return True
-    elif correct_result == 'BAD' and status == 'GOOD':
-        print "FAIL: Bad code compiled. Code:"
-        print_indented(code)
-        return False
-    elif correct_result == 'GOOD' and status = 'BAD':
-        print "FAIL: Good code didn't compile. Code:"
-        print_indented(code)
-        print "Compiler output:"
-        print_indented(output)
-    else: # Correct execution output was given
-        if status == "BAD":
-            print "FAIL: Good code didn't compile. Code:"
-            print_indented(code)
-            print "Compiler output:"
-            print_indented(output)
-        else:
-            print "FAIL: Incorrect output from execution. Code:"
-            print_indented(code)
-            print "Browser simulator output:"
-            print_indented(output)
-    """
     if correct_result == 'BAD':
         if status == 'BAD':
             print "PASS"
@@ -119,5 +82,3 @@ for line in test_file:
 test_file.close()
 
 print "%d / %d tests passed." % (pass_count, test_count)
-#print "Ran %d tests. %d%% passed. %d failed." % \
-#      (test_count, pass_count * 100 / test_count, test_count - pass_count)
