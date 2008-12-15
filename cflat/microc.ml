1473ff816c72ca1636e8918866fd236d8dff9e3f
Nicolas Viennot <nicolas@viennot.biz>
2008-12-15 12:09:25 -0500

    catch exception on syntax error

--- cflat/microc.ml
+++ cflat/microc.ml
@@ -1,5 +1,9 @@
 let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
-  let program_detail = Sast.check_program program in
-    print_string (Backend.generate_asm program_detail)
+  try
+    let lexbuf = Lexing.from_channel stdin in
+    let program = Parser.program Scanner.token lexbuf in
+    let program_detail = Sast.check_program program in
+      print_string (Backend.generate_asm program_detail); exit 0
+  with
+    Failure(s)          -> print_endline ("Error: " ^ s); exit 1
+  | Parsing.Parse_error -> print_endline ("Syntax error"); exit 1
