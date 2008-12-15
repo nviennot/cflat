3431e1bece5a97c7f04f3c1aaa05c0db27915b70
Nicolas Viennot <nicolas@viennot.biz>
2008-12-15 10:41:14 -0500

    ast printer removed

--- cflat/microc.ml
+++ cflat/microc.ml
@@ -1,10 +1,5 @@
-let print = false
-
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
   let program_detail = Sast.check_program program in
-  if print then
-    print_string (Printer.string_of_program program_detail)
-  else
     print_string (Backend.generate_asm program_detail)
