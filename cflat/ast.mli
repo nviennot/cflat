a913051e9522781baa4f102aafee68d92a058a62
Nicolas Viennot <nicolas@viennot.biz>
2008-12-20 11:53:28 -0500

    cleanup

--- cflat/ast.mli
+++ cflat/ast.mli
@@ -14,40 +14,40 @@ type incop =
     Pre_inc | Post_inc | Pre_dec | Post_dec
 
 type expr =
-    Literal of int
-  | Id of string
-  | Unop of unop * expr
-  | Incop of incop * string
-  | Binop of expr * binop * expr
+    Literal  of int
+  | Id       of string
+  | Unop     of unop * expr
+  | Incop    of incop * string
+  | Binop    of expr * binop * expr
   | Assignop of string * assignop * expr
-  | Call of string * expr list
+  | Call     of string * expr list
   | Noexpr
 
 type stmt =
-    Block of stmt list
-  | Expr of expr
-  | Return of expr
-  | If of expr * stmt * stmt
-  | For of expr * expr * expr * stmt
-  | While of expr * stmt
+    Block     of stmt list
+  | Expr      of expr
+  | Return    of expr
+  | If        of expr * stmt * stmt
+  | For       of expr * expr * expr * stmt
+  | While     of expr * stmt
   | Break
   | Continue
   | Try_catch of stmt * string * stmt
-  | Throw of expr
+  | Throw     of expr
 
 type func_decl = {
-  _fname : string;
+  _fname   : string;
   _formals : string list;
-  _body : stmt list;
+  _body    : stmt list;
 }
 
 type program = func_decl list
 
 type func_decl_detail = {
-  fname : string;
+  fname   : string;
   formals : string list;
-  locals : string list;
-  body : stmt list;
+  locals  : string list;
+  body    : stmt list;
 }
 
 type program_detail = func_decl_detail list
